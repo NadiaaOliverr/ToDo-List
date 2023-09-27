783620ea11a2584e5f64e86dd5ea1b7712b90ac7
Nádia Oliveira <nadiaaoliverr@gmail.com>
2023-09-27 10:24:13 -0300

    Adicionando funcionalidade dos botões de editar, remover e salvar

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldInputValue;
+
 // Funções
 
 function saveTodo(task){
@@ -45,6 +47,24 @@ function clearValue(){
     todoInput.focus();
 }
 
+function toggleForms(){
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+}
+
+function updateTodo(text){
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo) => {
+        let todoTitle = todo.querySelector("h3");
+
+        if(todoTitle.innerText === oldInputValue){
+            todoTitle.innerText = text;
+        }
+    });
+}
+
 // Eventos
 
 todoForm.addEventListener("submit", (e) => {
@@ -56,4 +76,46 @@ todoForm.addEventListener("submit", (e) => {
     if(inputValue){
         saveTodo(inputValue);  
     }
+})
+
+document.addEventListener("click", (e)=>{
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+    let todoTitle;
+
+    if(parentEl && parentEl.querySelector("h3")){
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
+
+    if(targetEl.classList.contains("finish-todo")){
+        parentEl.classList.toggle("done");
+    }
+
+    if(targetEl.classList.contains("remove-todo")){
+        parentEl.remove();
+    }
+
+    if(targetEl.classList.contains("edit-todo")){
+       toggleForms();
+       editInput.value = todoTitle;
+       oldInputValue = todoTitle;
+    }
+})
+
+cancelEditBtn.addEventListener("click", (e)=> {
+    e.preventDefault();
+
+    toggleForms();
+})
+
+editForm.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    const editInputValue = editInput.value;
+
+    if(editInputValue){
+        updateTodo(editInputValue)
+    }
+
+    toggleForms();
 })
